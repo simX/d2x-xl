be8518d2d9e141efe39b21f8892ac715a3901920
Philipp Nordhus <pnordhus@users.sourceforge.net>
2006-10-11 01:29:46 +0000

    Unnecessary files removed
    git-svn-id: https://d2x-xl.svn.sourceforge.net/svnroot/d2x-xl@94 01821946-851b-0410-b7f4-f1477c70e57a

--- branches/build-system/autogen.sh
+++ branches/build-system/autogen.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 set -e
 echo "If you get errors, you may need newer versions of automake and autoconf."
-echo "You'll need at least automake 1.5 and autoconf 2.50."
-aclocal $ACLOCAL_FLAGS
+echo "You'll need at least automake 1.7 and autoconf 2.50."
+
+WANT_AUTOMAKE=1.7 aclocal $ACLOCAL_FLAGS
 autoheader
 automake --add-missing --copy
 autoconf
+
 #./configure "$@"
 echo "Now you are ready to run ./configure"
