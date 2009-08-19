ab63073dbfb713f4ae1b0069b74a04570a1ac56a
d2x-xl <d2x-xl@01821946-851b-0410-b7f4-f1477c70e57a>
2009-08-19 11:29:45 +0000

    git-svn-id: https://d2x-xl.svn.sourceforge.net/svnroot/d2x-xl@5136 01821946-851b-0410-b7f4-f1477c70e57a

---
+++ tags/v1.14.101/io/args.cpp
@@ -0,0 +1,198 @@
+/*
+ THE COMPUTER CODE CONTAINED HEREIN IS THE SOLE PROPERTY OF PARALLAX
+ SOFTWARE CORPORATION ("PARALLAX").  PARALLAX, IN DISTRIBUTING THE CODE TO
+ END-USERS, AND SUBJECT TO ALL OF THE TERMS AND CONDITIONS HEREIN, GRANTS A
+ ROYALTY-FREE, PERPETUAL LICENSE TO SUCH END-USERS FOR USE BY SUCH END-USERS
+ IN USING, DISPLAYING,  AND CREATING DERIVATIVE WORKS THEREOF, SO LONG AS
+ SUCH USE, DISPLAY OR CREATION IS FOR NON-COMMERCIAL, ROYALTY OR REVENUE
+ FREE PURPOSES.  IN NO EVENT SHALL THE END-USER USE THE COMPUTER CODE
+ CONTAINED HEREIN FOR REVENUE-BEARING PURPOSES.  THE END-USER UNDERSTANDS
+ AND AGREES TO THE TERMS HEREIN AND ACCEPTS THE SAME BY USE OF THIS FILE.
+ COPYRIGHT 1993-1998 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <conf.h>
+#endif
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+
+#include "cfile.h"
+#include "u_mem.h"
+#include "strio.h"
+#include "strutil.h"
+#include "console.h"
+#include "error.h"
+#include "findfile.h"
+#include "strutil.h"
+
+int nArgCount = 0;
+
+#define MAX_ARGS	1000
+
+char * pszArgList [MAX_ARGS], *ps;
+
+//------------------------------------------------------------------------------
+
+int FindArg (const char * s)
+{
+	int i;
+  
+for (i = 0; i < nArgCount; i++)
+	if (pszArgList [i] && *pszArgList [i] && !stricmp (pszArgList [i], s))
+		return i;
+return 0;
+}
+
+//------------------------------------------------------------------------------
+
+void _CDECL_ args_exit (void)
+{
+	int i;
+
+PrintLog ("unloading program arguments\n");
+for (i = 0; i < nArgCount; i++)
+	if (pszArgList [i])
+		delete[] pszArgList [i];
+memset (pszArgList, 0, sizeof (pszArgList));
+nArgCount = 0;
+}
+
+//------------------------------------------------------------------------------
+
+char *GetIniFileName (char *fnIni, int bDebug)
+{
+	int	i;
+	char	*p;
+	CFile	cf;
+
+if ((i = FindArg ("-ini")))
+	strncpy (fnIni, pszArgList [i + 1], sizeof (fnIni) - 1);
+else {
+#if defined(__unix__)
+	FFS		ffs;
+	strcpy (fnIni, gameFolders.szHomeDir);
+	strcat (fnIni, "/.d2x-xl");
+	if (FFF (fnIni, &ffs, 0) <= 0) {
+#endif
+	strcpy (fnIni, gameFolders.szConfigDir);
+	i = strlen (fnIni);
+	if (i) {
+		p = fnIni + i - 1;
+		if ((*p == '\\') || (*p == '/'))
+			p++;
+		else {
+			strcat (fnIni, "/");
+			p += 2;
+			}
+		}
+	else
+		p = fnIni;
+	strcpy (p, bDebug ? "d2xdebug.ini" : "d2x.ini");
+	if (!cf.Exist (fnIni, "", false)) 
+		strcpy (p, bDebug ? "d2xdebug-default.ini" : "d2x-default.ini");
+#if defined(__unix__)
+   }
+#endif //!__unix__
+	}
+return fnIni;
+}
+
+//------------------------------------------------------------------------------
+
+void InitArgs (int argc, char **argv)
+{
+	int 		i, j;
+	CFile 	cf;
+	char 		*pszLine, *pszToken, fnIni [FILENAME_LEN];
+	static	char **pszArgs = NULL;
+	static	int  nArgs = 0;
+
+if (argv) {
+	pszArgs = argv;
+	nArgs = argc;
+	}
+else if (pszArgs) {
+	argv = pszArgs;
+	argc = nArgs;
+	}
+else
+	return;
+PrintLog ("Loading program arguments\n");
+args_exit ();
+for (i = 0; i < argc; i++)
+	pszArgList [nArgCount++] = StrDup (argv [i]);
+
+for (i = 0; i < nArgCount; i++)
+	if (pszArgList [i][0] == '-')
+		strlwr (pszArgList [i]);  // Convert all args to lowercase
+
+// look for the ini file
+// for unix, allow both ~/.d2x-xl and <config dir>/d2x.ini
+#if DBG
+GetIniFileName (fnIni, 1);
+#else
+GetIniFileName (fnIni, 0);
+#endif
+cf.Open (fnIni, "", "rt", 0);
+#if DBG
+if (!cf.File()) {
+	GetIniFileName (fnIni, 0);
+	cf.Open (fnIni, "", "rt", 0);
+	}
+#endif
+if (cf.File()) {
+	while (!cf.EoF ()) {
+		pszLine = fsplitword (cf, '\n');
+		if (*pszLine && (*pszLine != ';')) {
+			pszToken = splitword (pszLine, ' ');
+			if (nArgCount >= MAX_ARGS)
+				break;
+			pszArgList [nArgCount++] = pszToken;
+			if (pszLine) {
+				if (nArgCount >= MAX_ARGS) {
+					PrintLog ("too many program arguments\n");
+					break;
+					}
+				pszArgList [nArgCount++] = *pszLine ? StrDup (pszLine) : NULL;
+				}
+			}
+		delete[] pszLine; 
+		}
+	cf.Close ();
+	}
+PrintLog ("   ");
+for (i = j = 0; i < nArgCount; i++, j++) {
+	if (!pszArgList [i]) 
+		continue;
+	if ((pszArgList [i][0] == '-') && (isalpha (pszArgList [i][1]) || (j == 2))) {
+		PrintLog ("\n   ");
+		j = 0;
+		}
+	PrintLog (pszArgList [i]);
+	PrintLog (" ");
+	}
+PrintLog ("\n");
+atexit (args_exit);
+}
+
+// ----------------------------------------------------------------------------
+
+int NumArg (int t, int nDefault)
+{
+	char *psz = pszArgList [t+1];
+
+if (!psz)
+	return nDefault;
+if (psz && isdigit (*psz))
+	return atoi (psz);
+if (((*psz == '-') || (*psz == '+')) && isdigit (*psz + 1))
+	return atoi (psz);
+return nDefault;
+}
+
+//------------------------------------------------------------------------------
+//eof
---
+++ tags/v1.14.101/render/morph.cpp
@@ -0,0 +1,373 @@
+/*
+THE COMPUTER CODE CONTAINED HEREIN IS THE SOLE PROPERTY OF PARALLAX
+SOFTWARE CORPORATION ("PARALLAX").  PARALLAX, IN DISTRIBUTING THE CODE TO
+END-USERS, AND SUBJECT TO ALL OF THE TERMS AND CONDITIONS HEREIN, GRANTS A
+ROYALTY-FREE, PERPETUAL LICENSE TO SUCH END-USERS FOR USE BY SUCH END-USERS
+IN USING, DISPLAYING,  AND CREATING DERIVATIVE WORKS THEREOF, SO LONG AS
+SUCH USE, DISPLAY OR CREATION IS FOR NON-COMMERCIAL, ROYALTY OR REVENUE
+FREE PURPOSES.  IN NO EVENT SHALL THE END-USER USE THE COMPUTER CODE
+CONTAINED HEREIN FOR REVENUE-BEARING PURPOSES.  THE END-USER UNDERSTANDS
+AND AGREES TO THE TERMS HEREIN AND ACCEPTS THE SAME BY USE OF THIS FILE.
+COPYRIGHT 1993-1999 PARALLAX SOFTWARE CORPORATION.  ALL RIGHTS RESERVED.
+*/
+
+#ifdef HAVE_CONFIG_H
+#include <conf.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "descent.h"
+#include "texmap.h"
+#include "error.h"
+#include "light.h"
+#include "newdemo.h"
+#include "interp.h"
+
+//-------------------------------------------------------------
+//returns ptr to data for this CObject, or NULL if none
+tMorphInfo *MorphFindData (CObject *objP)
+{
+	int i;
+
+#ifdef NEWDEMO
+if (gameData.demo.nState == ND_STATE_PLAYBACK) {
+	gameData.render.morph.objects [0].objP = objP;
+	return &gameData.render.morph.objects [0];
+}
+#endif
+for (i = 0; i < MAX_MORPH_OBJECTS; i++)
+	if (gameData.render.morph.objects [i].objP == objP)
+		return gameData.render.morph.objects + i;
+return NULL;
+}
+
+//-------------------------------------------------------------
+//takes pmP, fills in min& max
+void MorphFindModelBounds (CPolyModel* pmP, int nSubModel, CFixVector& minv, CFixVector& maxv)
+{
+	ushort nVerts;
+	CFixVector *vp;
+	ushort *data, nType;
+
+	data = reinterpret_cast<ushort*> (pmP->Data () + pmP->SubModels ().ptrs [nSubModel]);
+	nType = *data++;
+	Assert (nType == 7 || nType == 1);
+	nVerts = *data++;
+	if (nType==7)
+		data+=2;		//skip start & pad
+	vp = reinterpret_cast<CFixVector*> (data);
+	minv = maxv = *vp++;
+	nVerts--;
+	while (nVerts--) {
+		if ((*vp)[X] > maxv[X])
+			maxv[X] = (*vp)[X];
+		else if((*vp)[X] < minv[X])
+			minv[X] = (*vp)[X];
+		if ((*vp)[Y] > maxv[Y])
+			maxv[Y] = (*vp)[Y];
+		else if ((*vp)[Y] < minv[Y])
+			minv[Y] = (*vp)[Y];
+		if ((*vp)[Z] > maxv[Z])
+			maxv[Z] = (*vp)[Z];
+		else if ((*vp)[Z] < minv[Z])
+			minv[Z] = (*vp)[Z];
+		vp++;
+	}
+}
+
+//-------------------------------------------------------------
+
+int MorphInitPoints (CPolyModel* pmP, CFixVector *vBoxSize, int nSubModel, tMorphInfo *mdP)
+{
+	ushort		nVerts;
+	CFixVector	*vp, v;
+	ushort		*data, nType;
+	int			i;
+
+//printf ("initing %d ", nSubModel);
+data = reinterpret_cast<ushort*> (pmP->Data () + pmP->SubModels ().ptrs [nSubModel]);
+nType = *data++;
+#if DBG
+//Assert (nType == 7 || nType == 1);
+if (nType != 7 && nType != 1)
+	return 0;
+#endif
+nVerts = *data++;
+mdP->nMorphingPoints [nSubModel] = 0;
+if (nType==7) {
+	i = *data++;		//get start point number
+	data++;				//skip pad
+	}
+else
+	i = 0;				//start at zero
+Assert (i+nVerts < MAX_VECS);
+mdP->submodelStartPoints [nSubModel] = i;
+vp = reinterpret_cast<CFixVector*> (data);
+v = *vp;
+while (nVerts--) {
+	fix k, dist;
+
+	if (vBoxSize) {
+		fix t;
+		k = 0x7fffffff;
+		if (v[X] && X2I ((*vBoxSize)[X]) < abs (v[X])/2 && (t = FixDiv ((*vBoxSize)[X], abs (v[X]))) < k)
+			k = t;
+		if (v[Y] && X2I ((*vBoxSize)[Y]) < abs (v[Y])/2 && (t = FixDiv ((*vBoxSize)[Y], abs (v[Y]))) < k)
+			k = t;
+		if (v[Z] && X2I ((*vBoxSize)[Z]) < abs (v[Z])/2 && (t = FixDiv ((*vBoxSize)[Z], abs (v[Z]))) < k)
+			k = t;
+		if (k == 0x7fffffff)
+			k = 0;
+		}
+	else
+		k = 0;
+	mdP->vecs[i] = *vp * k;
+	dist = CFixVector::NormalizedDir(mdP->deltas[i], *vp, mdP->vecs[i]);
+	mdP->times [i] = FixDiv (dist, gameData.render.morph.xRate);
+	if (mdP->times [i] != 0)
+		mdP->nMorphingPoints [nSubModel]++;
+		mdP->deltas [i] *= gameData.render.morph.xRate;
+	vp++;
+	i++;
+	}
+return 1;
+////printf ("npoints = %d\n", nMorphingPoints [nSubModel]);
+}
+
+//-------------------------------------------------------------
+
+int MorphUpdatePoints (CPolyModel* pmP, int nSubModel, tMorphInfo *mdP)
+{
+	ushort nVerts;
+	CFixVector *vp;
+	ushort *data, nType;
+	int i;
+
+	////printf ("updating %d ", nSubModel);
+data = reinterpret_cast<ushort*> (pmP->Data () + pmP->SubModels ().ptrs [nSubModel]);
+nType = *data++;
+#if DBG
+//Assert (nType == 7 || nType == 1);
+if (nType != 7 && nType != 1)
+	return 0;
+#endif
+nVerts = *data++;
+if (nType == 7) {
+	i = *data++;		//get start point number
+	data++;				//skip pad
+	}
+else
+	i = 0;				//start at zero
+vp = reinterpret_cast<CFixVector*> (data);
+while (nVerts--) {
+	if (mdP->times [i]) {		//not done yet
+		if ((mdP->times [i] -= gameData.time.xFrame) <= 0) {
+			 mdP->vecs [i] = *vp;
+			 mdP->times [i] = 0;
+			 mdP->nMorphingPoints [nSubModel]--;
+			}
+		else
+			mdP->vecs[i] += mdP->deltas[i] * gameData.time.xFrame;
+		}
+	vp++;
+	i++;
+	}
+return 1;
+////printf ("npoints = %d\n", nMorphingPoints [nSubModel]);
+}
+
+
+//-------------------------------------------------------------
+//process the morphing CObject for one frame
+void CObject::DoMorphFrame (void)
+{
+	int			i, t;
+	CPolyModel	*pmP;
+	tMorphInfo	*mdP;
+
+if (!(mdP = MorphFindData (this))) {	//maybe loaded half-morphed from disk
+	Die ();	//so kill it
+	return;
+	}
+pmP = gameData.models.polyModels [0] + mdP->objP->rType.polyObjInfo.nModel;
+G3CheckAndSwap (reinterpret_cast<void*> (pmP->Data ()));
+for (i = 0; i < pmP->ModelCount (); i++)
+	if (mdP->submodelActive [i] == 1) {
+		if (!MorphUpdatePoints (pmP, i, mdP))
+			mdP->submodelActive [i] = 0;
+      else if (mdP->nMorphingPoints [i] == 0) {		//maybe start submodel
+			mdP->submodelActive [i] = 2;		//not animating, just visible
+			mdP->nSubmodelsActive--;		//this one done animating
+			for (t = 0; t < pmP->ModelCount (); t++)
+				if (pmP->SubModels ().parents [t] == i) 		//start this one
+					if ((mdP->submodelActive [t] = MorphInitPoints (pmP, NULL, t, mdP)))
+						mdP->nSubmodelsActive++;
+			}
+		}
+if (!mdP->nSubmodelsActive) {			//done morphing!
+	CObject* objP = mdP->objP;
+	objP->info.controlType = mdP->saveControlType;
+	objP->info.movementType = mdP->saveMovementType;
+	objP->info.renderType = RT_POLYOBJ;
+	objP->mType.physInfo = mdP->savePhysInfo;
+	mdP->objP = NULL;
+	}
+}
+
+//-------------------------------------------------------------
+
+CFixVector morph_rotvel = CFixVector::Create(0x4000, 0x2000, 0x1000);
+
+void MorphInit ()
+{
+	int i;
+
+for (i = 0; i < MAX_MORPH_OBJECTS; i++)
+	gameData.render.morph.objects [i].objP = NULL;
+}
+
+
+//-------------------------------------------------------------
+//make the CObject morph
+void CObject::MorphStart (void)
+{
+	CPolyModel* pmP;
+	CFixVector pmmin, pmmax;
+	CFixVector vBoxSize;
+	int i;
+	tMorphInfo *mdP = &gameData.render.morph.objects [0];
+
+for (i = 0; i < MAX_MORPH_OBJECTS; i++, mdP++)
+	if (mdP->objP == NULL ||
+		 mdP->objP->info.nType == OBJ_NONE  ||
+		 mdP->objP->info.nSignature != mdP->nSignature)
+		break;
+
+if (i == MAX_MORPH_OBJECTS)		//no free slots
+	return;
+
+Assert (info.renderType == RT_POLYOBJ);
+mdP->objP = this;
+mdP->nSignature = info.nSignature;
+mdP->saveControlType = info.controlType;
+mdP->saveMovementType = info.movementType;
+mdP->savePhysInfo = mType.physInfo;
+Assert (info.controlType == CT_AI);		//morph OBJECTS are also AI gameData.objPs.objPects
+info.controlType = CT_MORPH;
+info.renderType = RT_MORPH;
+info.movementType = MT_PHYSICS;		//RT_NONE;
+mType.physInfo.rotVel = morph_rotvel;
+pmP = gameData.models.polyModels [0] + rType.polyObjInfo.nModel;
+G3CheckAndSwap (reinterpret_cast<void*> (pmP->Data ()));
+MorphFindModelBounds (pmP, 0, pmmin, pmmax);
+vBoxSize [X] = max (-pmmin [X], pmmax [X]) / 2;
+vBoxSize [Y] = max (-pmmin [Y], pmmax [Y]) / 2;
+vBoxSize [Z] = max (-pmmin [Z], pmmax [Z]) / 2;
+for (i = 0; i < MAX_VECS; i++)		//clear all points
+	mdP->times [i] = 0;
+for (i = 1; i < MAX_SUBMODELS; i++)		//clear all parts
+	mdP->submodelActive [i] = 0;
+mdP->submodelActive [0] = 1;		//1 means visible & animating
+mdP->nSubmodelsActive = 1;
+//now, project points onto surface of box
+MorphInitPoints (pmP, &vBoxSize, 0, mdP);
+}
+
+//-------------------------------------------------------------
+
+void MorphDrawModel (CPolyModel* modelP, int nSubModel, CAngleVector *animAngles, fix light, tMorphInfo *mdP, int nModel)
+{
+	int i, j, mn;
+	int facing;
+	int sort_list [MAX_SUBMODELS], sort_n;
+	//first, sort the submodels
+
+sort_list [0] = nSubModel;
+sort_n = 1;
+for (i = 0; i < modelP->ModelCount (); i++) {
+	if (mdP->submodelActive [i] && modelP->SubModels ().parents [i]==nSubModel) {
+		facing = G3CheckNormalFacing(modelP->SubModels ().pnts[i], modelP->SubModels ().norms[i]);
+		if (!facing)
+			sort_list [sort_n++] = i;
+		else {		//put at start
+			int t;
+			for (t=sort_n;t>0;t--)
+				sort_list [t] = sort_list [t-1];
+			sort_list [0] = i;
+			sort_n++;
+			}
+		}
+	}
+
+	//now draw everything
+for (i = 0; i < sort_n; i++) {
+	mn = sort_list [i];
+	if (mn == nSubModel) {
+		for (j = 0; j < modelP->TextureCount (); j++) {
+			gameData.models.textureIndex [j] = gameData.pig.tex.objBmIndex [gameData.pig.tex.objBmIndexP [modelP->FirstTexture () + j]];
+			gameData.models.textures [j] = gameData.pig.tex.bitmaps [0] + /*gameData.pig.tex.objBmIndex [gameData.pig.tex.objBmIndexP [modelP->FirstTexture ()+j]]*/gameData.models.textureIndex [j].index;
+			}
+
+#ifdef PIGGY_USE_PAGING
+		// Make sure the textures for this CObject are paged in..
+		gameData.pig.tex.bPageFlushed = 0;
+		for (j = 0; j < modelP->TextureCount (); j++)
+			LoadBitmap (gameData.models.textureIndex [j].index, 0);
+		// Hmmm.. cache got flushed in the middle of paging all these in,
+		// so we need to reread them all in.
+		if (gameData.pig.tex.bPageFlushed) {
+			gameData.pig.tex.bPageFlushed = 0;
+			for (int j = 0; j < modelP->TextureCount (); j++)
+				LoadBitmap (gameData.models.textureIndex [j].index, 0);
+			}
+			// Make sure that they can all fit in memory.
+		Assert ( gameData.pig.tex.bPageFlushed == 0);
+#endif
+		G3DrawMorphingModel (
+			modelP->Data () + modelP->SubModels ().ptrs [nSubModel],
+			gameData.models.textures,
+			animAngles, NULL, light,
+			mdP->vecs + mdP->submodelStartPoints [nSubModel], nModel);
+		}
+	else {
+		CFixMatrix orient;
+		orient = CFixMatrix::Create(animAngles[mn]);
+		transformation.Begin (modelP->SubModels ().offsets [mn], orient);
+		MorphDrawModel (modelP, mn, animAngles, light, mdP, nModel);
+		transformation.End ();
+		}
+	}
+}
+
+//-------------------------------------------------------------
+
+void CObject::MorphDraw (void)
+{
+//	int save_light;
+	CPolyModel* pmP;
+	fix light;
+	tMorphInfo *mdP;
+
+mdP = MorphFindData (this);
+Assert (mdP != NULL);
+Assert (rType.polyObjInfo.nModel < gameData.models.nPolyModels);
+pmP = gameData.models.polyModels [0] + rType.polyObjInfo.nModel;
+light = ComputeObjectLight (this, NULL);
+transformation.Begin (info.position.vPos, info.position.mOrient);
+G3SetModelPoints (gameData.models.polyModelPoints.Buffer ());
+gameData.render.vertP = gameData.models.fPolyModelVerts.Buffer ();
+MorphDrawModel (pmP, 0, rType.polyObjInfo.animAngles, light, mdP, rType.polyObjInfo.nModel);
+gameData.render.vertP = NULL;
+transformation.End ();
+
+#ifdef NEWDEMO
+if (gameData.demo.nState == ND_STATE_RECORDING)
+	NDRecordMorphFrame (mdP);
+#endif
+}
+
+//-------------------------------------------------------------
+//eof
